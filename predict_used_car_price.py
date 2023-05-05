a1e1198f3b0720c54a9398a3ac4cd33694083eae
Jacob Hajjar <jacob.hajjar1@gmail.com>
2023-05-05 15:25:51 -0700

    switched analysis to fully on spark

--- predict_used_car_price.py
+++ predict_used_car_price.py
@@ -3,12 +3,14 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-from pyspark import SparkConf, SparkContext
+
+from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 from pyspark.ml.regression import LinearRegression
-from sklearn.preprocessing import StandardScaler
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.evaluation import RegressionEvaluator  
+#from sklearn.preprocessing import StandardScaler
+from pyspark.ml.feature import VectorAssembler, StandardScaler, OneHotEncoder, StringIndexer
+from pyspark.ml.evaluation import RegressionEvaluator 
+from pyspark.sql.functions import col
 
 import matplotlib.pyplot as plt 
 import seaborn as sns                   
@@ -41,16 +43,80 @@ def load_process_data():
 
     return encoded_make_df
 
-def perform_regression(dataset):
+def load_data_spark(spark):
+    '''load and process the data using spark instead'''
+    data_path = "used_cars_labeled.csv"
+    df = spark.read.option("header",True).csv(data_path)
+    #set the make of the car data we want to load
+    cur_make = "Honda"
+    make_df = df[df['make'] == cur_make]
+    make_df  = make_df[['year','model','mileage','in_accident','1_owner','personal_used', 'price']]
+    #turn y value into int
+    make_df = make_df.withColumn('price',col('price').cast("int"))
+    #standardize the mileage
+    make_df = make_df.withColumn('mileage',col('mileage').cast("int"))
+    vectorAssembler = VectorAssembler(inputCols = ['mileage'], outputCol = 'vector_mileage')
+    make_df = vectorAssembler.transform(make_df)
+    std = StandardScaler(inputCol='vector_mileage', outputCol='scale_mileage')
+    make_df = std.fit(make_df).transform(make_df)
+    make_df.show()
+    #standardize the year
+    make_df = make_df.withColumn('year',col('year').cast("int"))
+    vectorAssembler = VectorAssembler(inputCols = ['year'], outputCol = 'vector_year')
+    make_df = vectorAssembler.transform(make_df)
+    std = StandardScaler(inputCol='vector_year', outputCol='scale_year')
+    make_df = std.fit(make_df).transform(make_df)
+    #old way of doing it in pandas
+    #make_df['mileage'] = std.fit_transform(make_df['mileage'].to_numpy().reshape(-1, 1))
+    #make_df['year'] = std.fit_transform(make_df['year'].to_numpy().reshape(-1, 1))
+    #convert t/f to bool
+    '''make_df['in_accident'] = make_df['in_accident'].astype('int')
+    make_df['1_owner'] = make_df['1_owner'].astype('int')
+    make_df['personal_used'] = make_df['1_owner'].astype('int')'''
+
+    make_df = make_df.withColumn('in_accident',col('in_accident').cast("boolean"))
+    make_df = make_df.withColumn('in_accident',col('in_accident').cast("integer"))
+
+    make_df = make_df.withColumn('1_owner',col('1_owner').cast("boolean"))
+    make_df = make_df.withColumn('1_owner',col('1_owner').cast("integer"))
+
+    make_df = make_df.withColumn('personal_used',col('personal_used').cast("boolean"))
+    make_df = make_df.withColumn('personal_used',col('personal_used').cast("integer"))
+
+    #make_df = make_df.select([col(c).cast("integer") for c ["test1", "test2"]])
+    make_df.show()
+    #drop models under 20 entries, then one hot encode
+    '''model_list = make_df['model'].value_counts()
+    for model, count in model_list.items():
+        if count < 20:
+            make_df = make_df[make_df['model'] != model]'''
+    #one hot encode all of the models
+    #encoded_make_df = pd.get_dummies(make_df, columns=['model'])
+    #vectorAssembler = VectorAssembler(inputCols = ['model'], outputCol = 'vector_model')
+    #make_df = vectorAssembler.transform(make_df)
+
+    stringIndexer = StringIndexer(inputCol="model", outputCol="model_index")
+    make_df = stringIndexer.fit(make_df).transform(make_df)
+    print(stringIndexer.params)
+    make_df.show()
+    encoder = OneHotEncoder(inputCol="model_index", outputCol='encoded_model')
+    encoded_make_df = encoder.fit(make_df).transform(make_df)
+    encoded_make_df.show()
+
+    return encoded_make_df
+
+
+def perform_regression(sqlContext, dataset):
     '''load the data and perform linear regression to predict the price and return the model'''
+    #get the data's columns and delete the prediction column which is price
+    #feature_columns = np.array(dataset.columns)
+    #feature_columns = np.delete(feature_columns, np.where(feature_columns == 'price')[0])
 
-    feature_columns = np.array(dataset.columns)
 
-    feature_columns = np.delete(feature_columns, np.where(feature_columns == 'price')[0])
-    conf = SparkConf().setMaster("local").setAppName("BigDataAnalysis")
-    sc = SparkContext(conf = conf)
-    sqlContext = SQLContext(sc)
-    make_spark_df = sqlContext.createDataFrame(dataset)
+    #make_spark_df = sqlContext.createDataFrame(dataset)
+    make_spark_df = dataset
+    feature_columns = ['in_accident', '1_owner', 'personal_used', 'scale_mileage', 'scale_year', 'encoded_model']
+    #feature_columns = ['in_accident', '1_owner', 'personal_used', 'scale_mileage', 'scale_year']
     vectorAssembler = VectorAssembler(inputCols = feature_columns, outputCol = 'features')
     make_spark_df = vectorAssembler.transform(make_spark_df)
     make_spark_df = make_spark_df.select(['features', 'price'])
@@ -87,15 +153,21 @@ def perform_regression(dataset):
     plt.show()
 
 
-
-
 def main():
     '''the main function'''
     os.environ['PYSPARK_PYTHON'] = sys.executable
-    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable 
-    make_df = load_process_data()
-    perform_regression(make_df)
-    
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+    #conf = SparkConf().setMaster("local").setAppName("BigDataAnalysis")
+    #sc = SparkContext(conf = conf)
+    #sqlContext = SQLContext(sc)
+
+    #create the spark session
+    spark = SparkSession.builder.master("local").appName("BigDataAnalysis").getOrCreate()
+    #enable pyarrow optimizations with spark when converting to and from dataframes
+    #spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+    #make_df = load_process_data()
+    make_df = load_data_spark(spark)
+    perform_regression(spark, make_df)
 
 if __name__ == '__main__':
     main()
